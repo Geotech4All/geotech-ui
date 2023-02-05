689d9d97fc2b62022fcefe56a34fadf460b1d11a
Ezekiel Victor <codeepoch@gmail.com>
2023-02-05 14:53:52 +0100

    changed nothing found for front facing

--- src/components/common/notFound/NotFound.tsx
+++ src/components/common/notFound/NotFound.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Link from "next/link";
 import Image from "next/image";
 import { getRandom } from "@utils/common";
 import { EmptyError, emptyErrors } from "@constants/errors";
@@ -12,11 +11,10 @@ interface NothingFoundProps {
 }
 
 export default function NothingFound(props: NothingFoundProps){
-  const { caption, isAdmin, url, name } = props;
+  const { caption } = props;
   const [emptyError, setEmptyError] = React.useState<EmptyError>()
 
   React.useEffect(() => setEmptyError(getRandom<EmptyError>(emptyErrors)), []);
-  const admin = isAdmin ? isAdmin : false;
   return (
     <div>
       <Image
@@ -27,10 +25,6 @@ export default function NothingFound(props: NothingFoundProps){
         alt={emptyError?.alt ?? ""}/>
       <div className="flex flex-col items-center justify-center text-lg text-black/60">
         <p>{caption}</p>
-        {url && admin && (
-          <Link
-            className="bg-red-400 transition-all hover:bg-red-500 rounded-md font-semibold p-1 text-white px-2"
-            href={url}>Add {name}</Link>)}
       </div>
     </div>
   );
