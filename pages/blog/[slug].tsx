f16d19bb0597dea869e0e0779a07e01efa9c1c38
Ezekiel Victor <codeepoch@gmail.com>
2023-02-06 11:17:14 +0100

    debuging post detail issue

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -24,6 +24,7 @@ const PostDetail: NextPageWithLayout = () => {
 
   React.useEffect(() => {
     if (!loading && !error) {
+      console.log(data?.post)
       if (data?.post) {
         setPost(data.post)
       }
