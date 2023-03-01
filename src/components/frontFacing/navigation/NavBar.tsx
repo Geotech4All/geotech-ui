3d30358554443ecd59658e638f51ede7ffe09fc0
Ezekiel Victor <codeepoch@gmail.com>
2023-03-01 16:15:44 +0100

    restyled user navigation

--- src/components/frontFacing/navigation/NavBar.tsx
+++ src/components/frontFacing/navigation/NavBar.tsx
@@ -5,8 +5,8 @@ import React from "react";
 
 export default function NavBar() {
   return (
-    <nav className="flex bg-black/20 text-white fixed z-[100] top-0 w-screen shadow-2xl backdrop-blur-sm">
-      <div className="shadow-inner shadow-black/30 gap-20 items-center flex w-full">
+    <nav className="flex fixed z-[100] top-0 w-screen shadow-2xl bg-white">
+      <div className="shadow-black/30 gap-20 items-center flex w-full">
         <Link href="/" className="max-w-[9rem] flex items-center ml-4"><FullLogo /></Link>
         <div className="flex px-10 items-center">
           {frontFacingNavUrl.map(url => (
