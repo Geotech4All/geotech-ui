9bf746ea69f40bf0ee5e2a1ccea27484f03223ed
Ezekiel Victor <codeepoch@gmail.com>
2023-05-21 19:32:03 +0100

    updated blog page

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -3,24 +3,30 @@ import { NavBarLayout } from "@components/frontFacing";
 import { NextPageWithLayout } from "@pages/_app";
 import { useAllPosts } from "@gql/requests/queries/hooks";
 import { CenterSLoadingRing, GImage, PostList, SomethingWentWrong } from "@components/common";
+import Head from "next/head";
 
 const Blog: NextPageWithLayout = () => {
-  const { loading, data, error } = useAllPosts({ first: 10 });
+    const { loading, data, error } = useAllPosts({ first: 10 });
 
-  if (loading) return <CenterSLoadingRing />;
-  if (error) return <SomethingWentWrong error={error} />;
-  if (data?.posts?.edges.length && data?.posts?.edges?.length < 1) return (
-    <div className="flex justify-center flex-col w-full items-center">
-      <GImage className="max-w-lg" src="/images/sorry.svg" alt="lady holding a sorry plack"/>
-      <h1 className="text-xl text-gray-600">Sorry no podcasts yet</h1>
-    </div>
-  );
-  
-  return (
-    <div className="p-3 flex flex-col gap-7">
-      <PostList title="Recent posts" className="w-full flex" posts={data?.posts}/>
-    </div>
-  );
+    if (loading) return <CenterSLoadingRing />;
+    if (error) return <SomethingWentWrong error={error} />;
+    if (data?.posts?.edges.length && data?.posts?.edges?.length < 1) return (
+        <div className="flex justify-center flex-col w-full items-center">
+            <GImage className="max-w-lg" src="/images/sorry.svg" alt="lady holding a sorry plack" />
+            <h1 className="text-xl text-gray-600">Sorry no podcasts yet</h1>
+        </div>
+    );
+
+    return (
+        <>
+            <Head>
+                <title>Blog - Geotech</title>
+            </Head>
+            <div className="p-3 flex flex-col gap-7">
+                <PostList title="Recent posts" className="w-full flex" posts={data?.posts} />
+            </div>
+        </>
+    );
 };
 
 Blog.getLayout = NavBarLayout;
