92159d2f038845d2a1fff9afa96682d8b82ea389
Ezekiel Victor <codeepoch@gmail.com>
2023-05-21 19:26:40 +0100

    updated blog styles

--- src/components/common/posts/PostReadLength.tsx
+++ src/components/common/posts/PostReadLength.tsx
@@ -8,7 +8,7 @@ export default function PostReadLength(props: PostReadLengthProps) {
   const readLength = Math.ceil((length / 60))
   return (
     <time
-      className="text-xs flex items-center"
+      className="text-xs whitespace-nowrap flex items-center"
       dateTime={length.toString()}>{readLength} min <span className="bg-gray-900 text-white p-0.5 px-2 rounded-xl">Read</span></time>
   )
 };
