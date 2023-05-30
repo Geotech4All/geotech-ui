5f0d9da750374c235577187fdb79db46be77e4c2
Ezekiel Victor <codeepoch@gmail.com>
2023-05-30 10:13:21 +0100

    updated hero text

--- src/components/frontFacing/Hero.tsx
+++ src/components/frontFacing/Hero.tsx
@@ -8,9 +8,12 @@ export default function Hero() {
                 <Earth />
             </Canvas>
             <div className="text-white absolute top-[64%] md:top-1/2 md:-translate-y-1/2 left-5 md:left-10">
-                <h1 className="text-5xl sm:text-6xl md:text-7xl font-extrabold">Geotech</h1>
-                <p className="text-xl">All things geoscience</p>
+                <h1 className="text-5xl sm:text-6xl md:text-7xl font-extrabold">Geotech4all</h1>
+                <div>
+                    <p className="text-xl">Everything geoscience</p>
+                    <p className="text-xl">News, Articles, Podcasts, Conferences, Webinars and Interviews</p>
+                </div>
             </div>
         </div>
     );
-};
+}
